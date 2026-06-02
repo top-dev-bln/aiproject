d3b3b736eb8e910d3fea68d2f555ee95eac607de
top.dev.bln <top.dev.bln@gmail.com>
2026-06-02 11:24:56 +0300

    adaugare masina

---
+++ src/main/java/com/arch/demo/Car.java
@@ -0,0 +1,22 @@
+package com.arch.demo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String passwordHash;
+    private boolean active;
+
+    public Long getId() { return id; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public boolean isActive() { return active; }
+}
